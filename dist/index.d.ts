abb88297cf3792d10a720c897b38291323571634
Grant Fortington <gwfortington@hotmail.com>
2024-08-12 18:15:23 +0930

    Implement "node-postgresql" update

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,3 @@
-import { Query } from 'node-postgresql';
 interface Options {
     configFilePath?: string;
     repositoryNumber?: number;
@@ -7,7 +6,8 @@ declare class Database {
     #private;
     private constructor();
     static getInstance(options?: Options): Database;
-    get query(): Query;
-    get transaction(): (callback: (query: Query) => Promise<void>) => Promise<void>;
+    get query(): (text: string, values?: any[]) => Promise<import("pg").QueryResult>;
+    get shutdown(): () => Promise<void>;
+    get transaction(): (callback: (query: (text: string, values?: any[]) => Promise<import("pg").QueryResult>) => Promise<void>) => Promise<void>;
 }
-export { Database, Options, Query };
+export { Database, Options };
--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,16 @@
 "use strict";
-var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, state, kind, f) {
-    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a getter");
-    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
-    return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
-};
 var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, state, value, kind, f) {
     if (kind === "m") throw new TypeError("Private method is not writable");
     if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a setter");
     if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot write private member to an object whose class did not declare it");
     return (kind === "a" ? f.call(receiver, value) : f ? f.value = value : state.set(receiver, value)), value;
 };
-var _a, _Database_instance;
+var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, state, kind, f) {
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a getter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
+    return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
+};
+var _a, _Database_instance, _Database_postgreSQL;
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Database = void 0;
 const node_debug_1 = require("node-debug");
@@ -20,6 +20,7 @@ let debug;
 const debugSource = 'database.class';
 class Database {
     constructor(options) {
+        _Database_postgreSQL.set(this, void 0);
         debug = new node_debug_1.Debug(debugSource);
         debug.write(node_debug_1.MessageType.Entry, options ? `options=${JSON.stringify(options)}` : undefined);
         debug.write(node_debug_1.MessageType.Step, 'Generating config options...');
@@ -46,8 +47,8 @@ class Database {
         debug.write(node_debug_1.MessageType.Step, 'Generating config...');
         const config = (0, node_postgresql_config_1.generate)(Object.keys(configOptions).length > 0 ? configOptions : undefined);
         debug.write(node_debug_1.MessageType.Value, `config=${JSON.stringify((0, node_postgresql_config_1.redacted)(config))}`);
-        debug.write(node_debug_1.MessageType.Step, 'Creating connection pool...');
-        (0, node_postgresql_1.createConnectionPool)(config);
+        debug.write(node_debug_1.MessageType.Step, 'Creating PostgreSQL instance...');
+        __classPrivateFieldSet(this, _Database_postgreSQL, node_postgresql_1.PostgreSQL.getInstance(config), "f");
         debug.write(node_debug_1.MessageType.Step, 'Setting "bigint" type parser...');
         node_postgresql_1.types.setTypeParser(node_postgresql_1.types.builtins.INT8, (value) => parseInt(value));
         debug.write(node_debug_1.MessageType.Step, 'Setting "decimal" type parser...');
@@ -68,12 +69,15 @@ class Database {
         return __classPrivateFieldGet(this, _a, "f", _Database_instance);
     }
     get query() {
-        return node_postgresql_1.query;
+        return __classPrivateFieldGet(this, _Database_postgreSQL, "f").query;
+    }
+    get shutdown() {
+        return __classPrivateFieldGet(this, _Database_postgreSQL, "f").shutdown;
     }
     get transaction() {
-        return node_postgresql_1.transaction;
+        return __classPrivateFieldGet(this, _Database_postgreSQL, "f").transaction;
     }
 }
 exports.Database = Database;
-_a = Database;
+_a = Database, _Database_postgreSQL = new WeakMap();
 _Database_instance = { value: void 0 };
